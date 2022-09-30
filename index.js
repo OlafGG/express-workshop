659e3d8a53d098ae3b46c046836498daceb459cc
OlafGG <baezauriel01@gmail.com>
2022-09-30 00:57:06 -0500

    new database and conection to mysql, new form to acces to database, i need to make other conections in the other get's

--- index.js
+++ index.js
@@ -1,46 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
-const { pokemon } = require('./pokedex.json');
+const pokemon = require('./routes/pokemon')
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res, next)=>{
     return res.status(200).send("Bienvenido al pokedex");
 });
 
-app.post("/pokemon", (req, res, next)=>{
-    return res.status(200).send(req.body);
-});
-
-app.get("/pokemon", (req, res, next)=>{
-    return res.status(200).send(pokemon)
-});
-
-app.get('/pokemon/:id([0-9]{1,3})', (req, res, next)=>{
-    const id = req.params.id - 1;
-    if(id >= 0 && id <= 150){
-        return res.status(200).send(pokemon[req.params.id - 1]);
-    } 
-    return res.status(404).send("Pokemon no encontrado");
-});
-
-app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
-    const name = req.params.name;
-
-    const pk = pokemon.filter((p)=>{
-        //Operador ternario
-        //condicion ? valor si verdadero : valor si falso
-        //Con el AND retorna la P
-        return (p.name.toUpperCase() == name.toUpperCase()) && p;
-    });
-
-    console.log(pk);
-    if(pk.length > 0){
-        return res.status(200).send(pk);
-    }
-    return res.status(404).send("Pokemon no encontrado");
-});
+app.use("/pokemon", pokemon);
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is running...");
