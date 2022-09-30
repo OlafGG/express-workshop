be67509131d04d93006f8109c71f4ec954542a97
OlafGG <baezauriel01@gmail.com>
2022-09-30 17:03:51 -0500

    Added SQL request and conection to database

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -11,26 +11,27 @@ pokemon.get("/", async (req, res, next)=>{
     return res.status(200).json(pkmn);
 });
 
-pokemon.get('/:id([0-9]{1,3})', (req, res, next)=>{
-    const id = req.params.id - 1;
-    if(id >= 0 && id <= 150){
-        return res.status(200).send(pk[req.params.id - 1]);
-    } 
+pokemon.get('/:id([0-9]{1,3})', async(req, res, next)=>{
+    const id = req.params.id;
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = " + id);
+    if(pkmn > 0){
+        return res.status(200).json(pkmn);
+    }
     return res.status(404).send("Pokemon no encontrado");
 });
 
-pokemon.get('/:name([A-Za-z]+)', (req, res, next) => {
+pokemon.get('/:name([A-Za-z]+)', async(req, res, next) => {
     const name = req.params.name;
-
-    const pkmn = pk.filter((p)=>{
+    
+    /*const pkmn = pk.filter((p)=>{
         //Operador ternario
         //condicion ? valor si verdadero : valor si falso
         //Con el AND retorna la P
         return (p.name.toUpperCase() == name.toUpperCase()) && p;
-    });
-
-    if(pkmn.length > 0){
-        return res.status(200).send(pkmn);
+    });*/
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name = '" + name.toLowerCase() + "'");
+    if(pkmn > 0){
+        return res.status(200).json(pkmn);
     }
     return res.status(404).send("Pokemon no encontrado");
 });
